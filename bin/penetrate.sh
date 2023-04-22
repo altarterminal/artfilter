c34f2b32a26844e8c00174e3aed5dd217d8ec7db
nm-jp <118420278+nm-jp@users.noreply.github.com>
2023-04-22 18:03:30 +0900

    implement basic function

--- bin/penetrate.sh
+++ bin/penetrate.sh
@@ -7,16 +7,19 @@ set -eu
 
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
-	Usage   : ${0##*/} -r<行数> -c<列数> -a<アナザーシーンファイル> [ワンシーンファイル]
-	Options : -s<起点> -d<増減>
+	Usage   : ${0##*/} -r<行数> -c<列数> -p<パラメータ> -a<別シーンファイル> [シーンファイル]
+	Options : -d<増減>
 
 	あるシーンの領域内に別シーンを上書きする。
-  ２つのシーンの幅と高さは一致する必要がある。
+	２つのシーンの幅と高さは一致する必要がある。
+
+	パラメータは以下の形式で指定する。
+	  左上座標を(x,y)、幅をw、高さをhとする長方形 -> "x,y,w,h"
 
 	-rオプションでフレームの行数を指定する。
 	-cオプションでフレームの列数を指定する。
+	-pオプションで領域のパラメータを指定する。
 	-aオプションでアナザーシーンファイルを指定する。
-	-sオプションで起点座標を指定する。デフォルトは"1,1"。
 	-dオプションで増減を指定する。デフォルトは"1,1"。
 	USAGE
   exit 1
@@ -30,8 +33,8 @@ print_usage_and_exit () {
 opr=''
 opt_r=''
 opt_c=''
+opt_p=''
 opt_a=''
-opt_s='1,1'
 opt_d='1,1'
 
 # 引数をパース
@@ -42,8 +45,8 @@ do
     -h|--help|--version) print_usage_and_exit ;;
     -r*)                 opt_r=${arg#-r}      ;;
     -c*)                 opt_c=${arg#-c}      ;;
+    -p*)                 opt_p=${arg#-p}      ;;
     -a*)                 opt_a=${arg#-a}      ;;
-    -s*)                 opt_s=${arg#-s}      ;;
     -d*)                 opt_d=${arg#-d}      ;;
     *)
       if [ $i -eq $# ] && [ -z "$opr" ]; then
@@ -80,6 +83,13 @@ if ! printf '%s\n' "$opt_c" | grep -Eq '^[0-9]+$'; then
   exit 31
 fi
 
+# 有効な数値であるか判定
+if ! printf '%s\n' "$opt_p"                    |
+   grep -Eq '^-?[0-9]+,-?[0-9]+,[0-9]+,[0-9]+$'; then
+  echo "${0##*/}: \"$opt_p\" invalid parameter" 1>&2
+  exit 31
+fi
+
 # 読み取り可能な通常ファイルであるか判定
 if   [ "_$opt_a" = '_' ] || [ "_$opt_a" = '_-' ]; then     
   echo "${0##*/}: forground content cannot be a file" 1>&2
@@ -92,14 +102,8 @@ else
 fi
 
 # 有効な数値の組であるか判定
-if ! printf '%s\n' "$opt_s" | grep -Eq '^-?[0-9]+,-?[0-9]+$'; then
-  echo "${0##*/}: \"$opt_s\" invalid number" 1>&2
-  exit 51
-fi
-
-# 有効な数値の組であるか判定
-if ! printf '%s\n' "$opt_d"                       |
-  grep -Eq '^-?[0-9](\.[0-9])?,-?[0-9](\.[0-9])?$'; then
+if ! printf '%s\n' "$opt_d"                        |
+   grep -Eq '^-?[0-9](\.[0-9])?,-?[0-9](\.[0-9])?$'; then
   echo "${0##*/}: \"$opt_d\" invalid number" 1>&2
   exit 51
 fi
@@ -108,8 +112,8 @@ fi
 onefile=$opr
 height=$opt_r
 width=$opt_c
+param=$opt_p
 anofile=$opt_a
-sp=$opt_s
 delta=$opt_d
 
 ######################################################################
@@ -121,14 +125,16 @@ BEGIN {
   # パラメータ設定
   height  = '"${height}"';
   width   = '"${width}"';
+  param   = "'"${param}"'";
   anofile = "'"${anofile}"'";
-  sp      = "'"${sp}"'";
   delta   = "'"${delta}"'";
 
-  # 増減を分離
-  split(sp, sary, ",");
-  sx = sary[1];
-  sy = sary[2];
+  # 領域パラメータを分離
+  split(param, pary, ",");
+  rx = pary[1];
+  ry = pary[2];
+  rw = pary[3];
+  rh = pary[4];
 
   # 増減を分離
   split(delta, dary, ",");
@@ -146,16 +152,42 @@ BEGIN {
   # パラメータを初期化
   rowidx = 1;
 
-  # 基準座標を初期化
-  cacx = sx;
-  cacy = sy;
-  cx   = int(cacx);
-  cy   = int(cacy);
+  # 現在の領域の範囲を設定
+  xcmin = rx;
+  xcmax = rx + rw - 1;
+  ycmin = ry;
+  ycmax = ry + rh - 1;
+
+  # 整数座標を取得
+  xcmini = intcoord(xcmin);
+  xcmaxi = intcoord(xcmax);
+  ycmini = intcoord(ycmin);
+  ycmaxi = intcoord(ycmax);
+
+  # 有効座標を取得
+  xcminv = validcoord(xcmini, width);
+  xcmaxv = validcoord(xcmaxi, width);
+  ycminv = validcoord(ycmini, height);
+  ycmaxv = validcoord(ycmaxi, height);
+
+  # フィルタ実行状態に遷移
+  state = "s_run";
 }
 
-{
-  if (rowidx == cy) {
-    $cx = "★";
+state == "s_run" {
+  # 現在の領域の位置を判断
+  if (xcmini > width  || xcmaxi < 1 ||
+      ycmini > height || ycmaxi < 1 ){
+    # 完全にフレーム外なので上書きしない
+  }
+  else {
+    # 一部または全部の領域がフレーム内にあるので上書きする
+
+    if (ycminv <= rowidx && rowidx <= ycmaxv) {
+      for (i = xcminv; i <= xcmaxv; i++) {
+        $i = anobuf[rowidx,i];
+      }
+    }
   }
 
   # 出力
@@ -164,14 +196,56 @@ BEGIN {
   # 行インデックスを更新
   rowidx++;
   if (rowidx > height) {
+    # フレームが終了
+
     # 行インデックスをリセット
     rowidx = 1;
 
-    # 基準座標を更新
-    cacx = cacx + dx;
-    cacy = cacy + dy;
-    cx = int(cacx);
-    cy = int(cacy);
+    # 現在の領域の範囲を更新
+    xcmin += dx;
+    xcmax += dx;
+    ycmin += dy;
+    ycmax += dy;
+
+    # 整数座標を更新
+    xcmini = intcoord(xcmin);
+    xcmaxi = intcoord(xcmax);
+    ycmini = intcoord(ycmin);
+    ycmaxi = intcoord(ycmax);
+
+    # 有効座標を更新
+    xcminv = validcoord(xcmini, width);
+    xcmaxv = validcoord(xcmaxi, width);
+    ycminv = validcoord(ycmini, height);
+    ycmaxv = validcoord(ycmaxi, height);
+
+    # 更新後に領域がフレーム外に出たら終了
+    if (dx > 0 && dy > 0 && (xcmini > width || ycmini > height) ||
+        dx > 0 && dy < 0 && (xcmini > width || ycmaxi < 1     ) ||
+        dx < 0 && dy > 0 && (xcmaxi < 1     || ycmini > height) ||
+        dx < 0 && dy < 0 && (xcmaxi < 1     || ycmaxi < 1     ) ){
+
+      # フィルタ終了状態に遷移
+      state = "s_fin";
+
+      # このパターンを終了
+      next;
+    }
   }
+}
+
+# 有効座標に修正（フレーム範囲内の座標に修正）
+function validcoord(ival,max) {
+  return (ival < 1) ? 1 : (ival > max) ? max : ival;
+}
+
+# 整数座標（四捨五入）に修正
+function intcoord(val) {
+  return int(val + 0.5);
+}
+
+state == "s_fin" {
+  # 入力をパススルー
+  print;
 }
 ' ${onefile:+"$onefile"}
