e7bee6c593835400256350e7350d9b4cc1bffc87
nm-jp <118420278+nm-jp@users.noreply.github.com>
2023-05-13 23:23:24 +0900

    change the interface of underwrite

--- bin/underwrite.sh
+++ bin/underwrite.sh
@@ -7,15 +7,16 @@ set -eu
 
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
-	Usage   : ${0##*/} -r<行数> -b<背景ファイル> [前景ファイル]
-	Options : -o<オフセット> -c<透過部分のマーカー>
+	Usage   : ${0##*/} -r<行数> -c<列数> -b<背景ファイル> [前景ファイル]
+	Options : -o<オフセット> -t<透過文字>
 
 	背景に対して前景を上書きする。
 
 	-rオプションでフレームの行数を指定する。
+	-cオプションでフレームの列数を指定する。
 	-fオプションで前景のファイルを指定する。
 	-oオプションで背景のオフセットを指定できる。デフォルトは"0,0"。
-	-cオプションで前景中の透過領域を示す文字を指定できる。デフォルトは□。
+	-tオプションで前景中の透過領域を示す文字を指定できる。デフォルトは□。
 	USAGE
   exit 1
 }
@@ -27,9 +28,10 @@ print_usage_and_exit () {
 # 変数を初期化
 opr=''
 opt_r=''
+opt_c=''
 opt_b=''
 opt_o='0,0'
-opt_c='□'
+opt_t='□'
 
 # 引数をパース
 i=1
@@ -38,9 +40,10 @@ do
   case "$arg" in
     -h|--help|--version) print_usage_and_exit ;;
     -r*)                 opt_r=${arg#-r}      ;;
+    -c*)                 opt_c=${arg#-c}      ;;
     -b*)                 opt_b=${arg#-b}      ;;
     -o*)                 opt_o=${arg#-o}      ;;
-    -c*)                 opt_c=${arg#-c}      ;;
+    -t*)                 opt_t=${arg#-t}      ;;
     *)
       if [ $i -eq $# ] && [ -z "$opr" ]; then
         opr=$arg
@@ -70,13 +73,19 @@ if ! printf '%s\n' "$opt_r" | grep -Eq '^[0-9]+$'; then
   exit 31
 fi
 
+# 有効な数値であるか判定
+if ! printf '%s\n' "$opt_c" | grep -Eq '^[0-9]+$'; then
+  echo "${0##*/}: \"$opt_c\" invalid number" 1>&2
+  exit 41
+fi
+
 # 読み取り可能な通常ファイルであるか判定
 if   [ "_$opt_b" = '_' ] || [ "_$opt_b" = '_-' ]; then     
   echo "${0##*/}: forground content cannot be a file" 1>&2
-  exit 41
+  exit 51
 elif [ ! -f "$opt_b"   ] || [ ! -r "$opt_b"    ]; then
   echo "${0##*/}: \"$opt_b\" cannot be opened" 1>&2
-  exit 42
+  exit 52
 else
   :
 fi
@@ -84,21 +93,22 @@ fi
 # 有効な数値の組であるか判定
 if ! printf '%s\n' "$opt_o" | grep -Eq '^-?[0-9]+,-?[0-9]+$'; then
   echo "${0##*/}: \"$opt_o\" invalid number" 1>&2
-  exit 51
+  exit 61
 fi
 
-# 引数を評価
-if printf '%s\n' "$opt_c" | grep -q '^$'; then
+# 有効な文字列であるか判定
+if ! printf '%s\n' "$opt_t" | grep -q '^.$'; then
   echo "${0##*/}: transparent character must be specified" 1>&2
-  exit 61
+  exit 71
 fi
 
 # パラメータを決定
 forfile=$opr
 height=$opt_r
+width=$opt_c
 backfile=$opt_b
 offset=$opt_o
-tchar=$opt_c
+tchar=$opt_t
 
 ######################################################################
 # 本体処理
@@ -107,6 +117,7 @@ tchar=$opt_c
 gawk -v FS='' -v OFS='' '
 BEGIN {
   height   = '"${height}"';
+  width    = '"${width}"';
   backfile = "'"${backfile}"'";
   offset   = "'"${offset}"'";
   tchar    = "'"${tchar}"'";
@@ -130,18 +141,17 @@ BEGIN {
 
   # パラメータを初期化
   rowidx = 1;
+
+  # 水平方向の表示範囲を確認
+  sidx = (ox < 1) ? 1 : (ox+1);
+  eidx = (width < (ox+bwidth)) ? width : (ox+bwidth);
 }
 
 {
   # 背景領域の中にあるときは上書き
   if ((oy < rowidx) && (rowidx <= oy+bheight)) {
-    sidx = (ox < 1) ? 1 : (ox+1);
-    eidx = ((ox+bwidth) < NF) ? (ox+bwidth) : NF;
-
     for (i = sidx; i <= eidx; i++) {
-      if ($i == tchar) {
-        $i = bbuf[rowidx-oy,i-ox];
-      }
+      if ($i == tchar) { $i = bbuf[rowidx-oy,i-ox]; }
     }
   }
 
